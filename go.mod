f18ec8ab4b1a0ea934fccc76eb8353edfcb9b40e
yongj.zhuang <yongj.zhuang@outlook.com>
2024-06-28 11:27:21 +0800

    Import miso 40ac7296d9ae0a6cb357b1d92fe4f9edfaa93dd2

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module requirement
 
 go 1.20
+
+require github.com/curtisnewbie/miso v0.1.3-0.20240628031452-40ac7296d9ae
--- requirement.go
+++ requirement.go
@@ -3,11 +3,11 @@ package requirement
 import (
 	"flag"
 	"fmt"
-	"io"
 	"os"
-	"os/exec"
 	"regexp"
 	"strings"
+
+	"github.com/curtisnewbie/miso/util"
 )
 
 const (
@@ -19,6 +19,14 @@ const (
 	green         = "\033[1;32m"
 	cyan          = "\033[1;36m"
 	redBlink      = "\033[1;5;31m"
+)
+
+const (
+	codeBlock = "```"
+)
+
+const (
+	EnvRequirementFile = "REQUIREMENTS_FILE"
 )
 
 var (
@@ -58,23 +66,22 @@ func (r *Requirement) ResetFlags() {
 	r.InTodos = false
 }
 
-func (r Requirement) AddCodeBlock(s string) Requirement {
+func (r *Requirement) AddCodeBlock(s string) {
 	s = strings.TrimSpace(s)
 	i := strings.Index(s, "\n")
 	if i > -1 {
 		s = s[i+1:]
 	}
-	s, _ = strings.CutSuffix(s, "```")
+	s, _ = strings.CutSuffix(s, codeBlock)
 	sp := strings.Split(s, "\n")
 	s = "  " + strings.Join(sp, "\n  ")
 
 	r.CodeBlocks = append(r.CodeBlocks, s)
-	return r
 }
 
 var bracketPat = regexp.MustCompile(`[【】]`)
 
-func (r Requirement) SetName(s string) Requirement {
+func (r *Requirement) SetName(s string) {
 	r.Name = bracketPat.ReplaceAllStringFunc(s, func(s string) string {
 		switch s {
 		case "【":
@@ -85,7 +92,6 @@ func (r Requirement) SetName(s string) Requirement {
 			return s
 		}
 	})
-	return r
 }
 
 func (r Requirement) String() string {
@@ -143,9 +149,9 @@ func (r Requirement) String() string {
 	return s
 }
 
-func NewRequirement(name string) Requirement {
+func NewRequirement(name string) *Requirement {
 	r := Requirement{}
-	r = r.SetName(name)
+	r.SetName(name)
 	r.Docs = []string{}
 	r.Repos = []string{}
 	r.Branches = []string{}
@@ -153,7 +159,7 @@ func NewRequirement(name string) Requirement {
 	r.BranchMatched = -1
 	r.RepoMatched = -1
 	r.CodeBlocks = []string{}
-	return r
+	return &r
 }
 
 func ParseTilde(v string) string {
@@ -170,13 +176,11 @@ func ParseRequirements() {
 	var branch string
 	var cwd string
 
-	cmd := exec.Command("git", "status")
-	var cmdout []byte
-	var err error
-	if cmdout, err = cmd.CombinedOutput(); err != nil {
-		fmt.Printf("%sNot a git repository, %v%s\n", red, err, reset)
+	cmdout, err := util.CliRun("git", "status")
+	if err != nil {
+		util.Printlnf("%sNot a git repository, %v%s", red, err, reset)
 	} else {
-		outstr := UnsafeByt2Str(cmdout)
+		outstr := util.UnsafeByt2Str(cmdout)
 		lines := strings.Split(outstr, "\n")
 		if len(lines) < 1 {
 			fmt.Printf("%sNot a git repository%s", red, reset)
@@ -188,7 +192,7 @@ func ParseRequirements() {
 				branch = string([]rune(lines[0])[l+10:])
 			}
 		}
-		fmt.Printf("On Branch %v%v%v\n", red, branch, reset)
+		util.Printlnf("On Branch %v%v%v", red, branch, reset)
 
 		cw, err := os.Getwd()
 		if err != nil {
@@ -202,21 +206,15 @@ func ParseRequirements() {
 				break
 			}
 		}
-		fmt.Printf("Current Repo: %v%s%v\n", red, cwd, reset)
+		util.Printlnf("Current Repo: %v%s%v", red, cwd, reset)
 	}
 
-	reqFileName := os.Getenv("REQUIREMENTS_FILE")
-	reqFile, err := os.Open(reqFileName)
+	byt, err := util.ReadFileAll(os.Getenv(EnvRequirementFile))
 	if err != nil {
 		panic(err)
 	}
 
-	byt, err := io.ReadAll(reqFile)
-	if err != nil {
-		panic(err)
-	}
-
-	content := UnsafeByt2Str(byt)
+	content := util.UnsafeByt2Str(byt)
 	i := strings.Index(content, "## Active Requirements")
 	if i > 0 {
 		for j := i; j < len(content); j++ {
@@ -234,12 +232,9 @@ func ParseRequirements() {
 	if len(splited) < 1 {
 		fmt.Printf("%sRequirement not found%s", red, reset)
 	}
-	requirements := make([]Requirement, 0, 30)
-	codeBlock := "```"
+	requirements := util.NewStack[*Requirement](30)
 
-	start := 0
-
-	for i := start; i < len(splited); i++ {
+	for i := 0; i < len(splited); i++ {
 		l := splited[i]
 
 		if strings.HasPrefix(l, "#") || strings.TrimSpace(l) == "" {
@@ -254,10 +249,11 @@ func ParseRequirements() {
 			}
 			l = strings.Join(splited[i:j+1], "\n")
 
-			if len(requirements) < 1 {
+			curr, ok := requirements.Peek()
+			if !ok {
 				panic(fmt.Errorf("illegal format, line: %v", l))
 			}
-			requirements[len(requirements)-1] = requirements[len(requirements)-1].AddCodeBlock(l)
+			curr.AddCodeBlock(l)
 			i = j + 1
 			continue
 		}
@@ -267,12 +263,12 @@ func ParseRequirements() {
 			if strings.TrimSpace(matched[1]) != "" {
 				curr.Done = true
 			}
-			requirements = append(requirements, curr)
+			requirements.Push(curr)
 		} else {
-			if len(requirements) < 1 {
+			curr, ok := requirements.Peek()
+			if !ok {
 				panic(fmt.Errorf("illegal format, line: %v", l))
 			}
-			curr := requirements[len(requirements)-1]
 
 			if matched := docRegex.FindStringSubmatch(l); len(matched) > 0 {
 				curr.ResetFlags()
@@ -303,22 +299,20 @@ func ParseRequirements() {
 					}
 				}
 			}
-			requirements[len(requirements)-1] = curr
 		}
 	}
-	// fmt.Println(requirements)
 
 	isMain := branch == "master" || branch == "main"
-	requirements = Filter(requirements, func(r Requirement) bool { return !r.Done })
+	filtered := util.Filter(requirements.Slice(), func(r *Requirement) bool { return !r.Done })
 
 	if *flagAll || (branch == "") {
-		fmt.Printf("Found %v%v%v Requirements\n\n", red, len(requirements), reset)
-		for _, req := range requirements {
-			fmt.Printf("%v\n", req)
+		util.Printlnf("Found %v%v%v Requirements\n", red, len(filtered), reset)
+		for _, req := range filtered {
+			util.Printlnf("%v", req)
 		}
 	} else {
-		mr := []Requirement{}
-		for _, req := range requirements {
+		mr := make([]*Requirement, 0, len(filtered))
+		for _, req := range filtered {
 			matched := false
 			for i, v := range req.Repos {
 				if strings.Contains(v, cwd) {
@@ -339,25 +333,10 @@ func ParseRequirements() {
 			mr = append(mr, req)
 		}
 
-		fmt.Printf("Found %v%v%v Requirements, Matched %v%v%v Requirements\n\n", red, len(requirements), reset, red, len(mr), reset)
+		util.Printlnf("Found %v%v%v Requirements, Matched %v%v%v Requirements\n", red, len(filtered), reset, red, len(mr), reset)
 		for _, req := range mr {
-			fmt.Printf("%v\n", req)
+			util.Printlnf("%v", req)
 		}
 	}
 
-}
-
-func Filter[T any](l []T, f func(T) bool) []T {
-	cp := l[:0]
-	for i := range l {
-		x := l[i]
-		if f(x) {
-			cp = append(cp, x)
-		}
-	}
-	for i := len(cp); i < len(l); i++ {
-		var nt T
-		l[i] = nt
-	}
-	return cp
 }
